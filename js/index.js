29ae2455795e4be25b63bbc69abbccd8538122e4
Rodina Ibrahim <99216903+rodina77@users.noreply.github.com>
2022-06-03 21:04:13 +0200

    version-1

---
+++ js/index.js
@@ -0,0 +1,46 @@
+
+
+  $('#firr').modal('hide');
+  $('#first').modal('hide');
+  $('#second').modal('hide');
+  $('#third').modal('hide');
+  $('#fourth').modal('hide');
+  $('#fifth').modal('hide');
+  $('#sixth').modal('hide');
+  $('#seventh').modal('hide');
+ 
+  $("#bttn").click(function(){
+    $('#firr').modal('show');
+  });
+  $("#bttn1").click(function(){
+    $('#first').modal('show');
+  });
+  $("#bttn2").click(function(){
+    $('#second').modal('show');
+  });
+  $("#bttn3").click(function(){
+    $('#third').modal('show');
+  });
+  $("#bttn4").click(function(){
+    $('#fourth').modal('show');
+  });
+  $("#bttn5").click(function(){
+    $('#fifth').modal('show');
+  });
+  $("#bttn6").click(function(){
+    $('#sixth').modal('show');
+  });
+  $("#bttn7").click(function(){
+    $('#seventh').modal('show');
+  });
+
+  $(".actions").click(function(){
+    $('#first').modal('hide');
+    $('#firr').modal('hide');
+    $('#second').modal('hide');
+    $('#third').modal('hide');
+    $('#fourth').modal('hide');
+    $('#fifth').modal('hide');
+    $('#sixth').modal('hide');
+    $('#seventh').modal('hide');
+  });
